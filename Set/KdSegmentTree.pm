fa57cec7bc51bea2ca6daea00a5d0c1e15334491
Jaguar Xiong <xiong.jaguar@gmail.com>
2011-10-17 19:47:38 +0800

    bug fix

--- Set/KdSegmentTree.pm
+++ Set/KdSegmentTree.pm
@@ -1,4 +1,3 @@
-#!/usr/bin/perl
 
 package Set::KdSegmentTree;
 
@@ -7,7 +6,7 @@ require Exporter;
 @ISA    = qw(Exporter);
 @EXPORT = qw(add_interval make_tree
   query_point query_overlap query_window
-  debug_dump
+  debug_dump_tree
 );
 
 use strict;
@@ -34,13 +33,30 @@ sub _bld_bintree {
         my ( $values, $len, $idx, $sorted, $order) = @_;
         $order = _trv_c( $values, $len, 2 * $idx, $sorted, $order) if 2 * $idx <= $len;
         my $val = $values->[$order-1];
-        $sorted->[$idx] = [$val,0,0];
+        $sorted->[$idx] = [$val,0,0,0,0];
         $order ++;
         $order = _trv_c( $values, $len, 2 * $idx + 1, $sorted, $order) if 2 * $idx + 1 <= $len;
         return $order;
     }
     _trv_c( $values, $len, 1, \@_sorted, 1);
-    #map { push( @_sorted, [] ); } ( 1 .. ( 1+$len ) );
+
+    sub _leaf_min {
+        my ($values, $len, $idx) = @_;
+        return _leaf_min($value, $len, 2*$idx) if (2 * $idx <= $len);
+        my $node = $value->[$idx];
+        return $node->[0];
+    }
+    sub _leaf_max {
+        my ($values, $len, $idx) = @_; 
+        return _leaf_max($value, $len, 2*$idx+1) if (2 * $idx + 1<= $len);
+        my $node = $value->[$idx];
+        return $node->[0];
+    }
+    map {
+        my $node = $_sorted[$_];
+        $node->[3] = _leaf_min(\@_sorted, $len, $_);
+        $node->[4] = _leaf_max(\@_sorted, $len, $_);
+    } (1..$len);
     return \@_sorted;
 }
 
@@ -62,16 +78,19 @@ sub _add_val {
         return;
     }
 
-    #printf("%2d [%5d-%5d-%5d] [%d %d]\n", $idx, $min, $sep, $max, $a, $b);
+    if ( $a == $sep and $sep == $b) {
+        #only one node
+        $node->[2] = [] if (0 eq $node->[2]);
+        push( @{ $node->[2] }, $value );
+        return;
+    }
     if ( $a == $sep or $sep == $b ) {
         $node->[2] = [] if $node->[2] == undef;
         push( @{ $node->[2] }, $value );
-        #printf("%2d [%5d-%5d-%5d] [%d %d] - ext\n", $idx, $min, $sep, $max, $a, $b);
     }
     if ( $a <= $min and $max <= $b ) {
         $node->[1] = [] if $node->[1] == undef;
         push( @{ $node->[1] }, $value );
-        #printf("%2d [%5d-%5d-%5d] [%d %d] - ext\n", $idx, $min, $sep, $max, $a, $b);
         return;
     }
     _add_val( $bin_tree, 2 * $idx, $min, $sep, $value, $lvl )
@@ -86,6 +105,8 @@ sub make_tree {
     my $max_level = $self->{max_level};
     my @que       = ();
     my $subtrees = [];
+    my $debug = $self->{debug};
+
     push( @que, [ $values, 0, $subtrees] );
     while ( scalar(@que) > 0 ) {
         my $item        = pop(@que);
@@ -106,17 +127,14 @@ sub make_tree {
             printf( ": %d\n", scalar(@points) );
         }
         my $key_cnt = scalar(@points);
-        #map { print @points->[$_],','} (0..scalar(@points)-1); printf "\n";
         my $sorted = _bld_bintree( \@points, scalar(@points) );
         map {
-            _add_val( $sorted, 1, @points[0], @points[ $key_cnt - 1 ],
-                $_, $lvl );
+            my $min = scalar($points[0]);
+            my $max = scalar($points[$key_cnt-1]);
+            _add_val( $sorted, 1, $min, $max, $_, $lvl);
         } @$boxes;
-        #push(@$subtrees, $sorted);
-        #printf("tree: %x, len %x\n", $sorted,$sorted->[0]);
         push(@$subcont, $sorted);
 
-        #printf( "level %d: %d\n", $lvl, scalar(@$sorted) ) if $self->{debug};
         map {
             my $node      = $sorted->[ $_];
             my $sub_match = $node->[1];
@@ -136,12 +154,12 @@ sub make_tree {
             }
 
             if ( 1+$lvl < $max_level ) {
-                if ( undef != $sub_match ) {
+                if (defined($sub_match) and (0 != $sub_match) ) {
                     my $subtree = [];
                     push( @que, [ $sub_match, $lvl + 1, $subtree] );
                     $node->[1] = $subtree;
                 }
-                if ( undef != $ext_match ) {
+                if (defined($ext_match) and (0 != $ext_match) ) {
                     my $subtree = [];
                     push( @que, [ $ext_match, $lvl + 1, $subtree] );
                     $node->[2] = $subtree;
@@ -158,32 +176,49 @@ sub _query_tree {
     my $loc = $points->[$lvl];
     my @ret = ();
     my @que = (1);
+    my $len = $tree->[0];
     while (scalar(@que)>0) {
         my $idx = pop(@que);
-        my $len = $tree->[0];
         my $node = $tree->[$idx];
         my $sep = $node->[0];
-        #printf(" %8x %2d %5d : %d %x %x\n", $tree, $idx, $sep, $loc,
-        #        $node->[1], $node->[2]);
-        push(@ret, $node->[1]->[0])if $node->[1] != undef;
-        if ($loc == $sep) {
-            push(@ret, $node->[2]->[0])if $node->[2] != undef;
-        }
-        if ($loc < $sep) {
-            push(@que, 2*$idx) if 2*$idx<=$len;
-        } elsif ($loc > $sep) {
-            push(@que, 2*$idx+1) if 1+2*$idx<=$len;
+        my $min = $node->[3];
+        my $max = $node->[4];
+        if ($min <= $loc and $loc <= $max) {
+            if (defined($node->[1]) and (0 != $node->[1])) {
+                if ($lvl+1 < $max_lvl) {
+                    push(@ret, $node->[1]->[0]);
+                } else {
+                    my $vals = $node->[1];
+                    map { push(@ret, $_) } @$vals;
+                }
+            }
+            if ($loc == $sep) {
+                if (defined($node->[2]) and (0 != $node->[2])) {
+                    if (defined($node->[2]) and (0 != $node->[2])) {
+                        push(@ret, $node->[2]->[0]);
+                    } else {
+                        my $vals = $node->[2];
+                        map { push(@ret, $_) } @$vals;
+                    }
+                }
+            }
+            if ($loc < $sep) {
+                push(@que, 2*$idx) if 2*$idx<=$len;
+            } elsif ($loc > $sep) {
+                push(@que, 2*$idx+1) if 1+2*$idx<=$len;
+            }
         }
     }
-    #printf(" %8x          : %d ret\n", $tree, scalar(@ret));
     return \@ret;
 }
 
 sub query_point {
     my ( $self, $points ) = @_;
     my $subtree = $self->{subtree};
+    my $max_lvl = $self->{max_level};
     my @que = ();
-    my @ret = ();
+    my @ret = {};
+    my $debug = $self->{debug};
     push(@que, [$subtree, 0]);
     while ( scalar(@que) > 0 ) {
         my $item = pop(@que);
@@ -191,15 +226,16 @@ sub query_point {
         my $lvl  = $item->[1];
         my $trees = _query_tree($tree, $lvl, $points);
         map {
-            if ($lvl+1 < $self->{max_level}) {
-                #printf(" %8x          : %d lvl\n", $_, $lvl+1);
+            if ($lvl+1 < $max_lvl) {
+                printf(" %8x : %d lvl\n", $_, $lvl+1) if $debug;
                 push(@que, [$_, $lvl+1]) ;
             } else {
-                #printf(" %8x          : \n", $_);
-                push(@ret, $_);
+                print $_, "\n" if $debug;
+                $ret->{$_} = $_->[1];
             }
         } @$trees;
     }
+    my @boxes = values %$ret;
     return \@ret;
 }
 
@@ -226,20 +262,35 @@ sub debug_dump {
         my $len = $tree->[0];
         printf("tree : %x (%d internal nodes, %d total)\n", $tree, 
                 $len, scalar(@$tree));
+
         for my $idx (1..$len) {
             my $node = $tree->[$idx];
-            printf(" node-%02d %5d", $idx, $node->[0]);
-            if (undef != $node->[1]) {
-                my $subtree = $node->[1][0];
-                printf(" (%8x %8x)", $subtree, $subtree->[0]);
-                push(@que, [$subtree, $lvl+1]) if (1+$lvl < $self->{max_level});
+            printf(" node-%02d %5d [%d,%d]", $idx, $node->[0], $node->[3], $node->[4]);
+            if (defined($node->[1]) and (0 != $node->[1])) {
+                if (1+$lvl < $self->{max_level}) {
+                    my $subtree = $node->[1][0];
+                    printf(" (%8x %8x)", $subtree, $subtree->[0]);
+                    push(@que, [$subtree, $lvl+1]);
+                } else {
+                    my $vals = $node->[1];
+                    printf(" (%08x ", $vals);
+                    map {  print '"',$_->[1], '",'} @$vals;
+                    print ")";
+                }
             } else {
                 printf(" (%8x %8x)", 0, 0);
             }
-            if (undef != $node->[2]) {
-                my $subtree = $node->[2][0];
-                printf(" (%8x %8x)", $subtree, $subtree->[0]);
-                push(@que, [$subtree, $lvl+1]) if (1+$lvl < $self->{max_level});
+            if (defined($node->[2]) and (0 != $node->[2])) {
+                if (1+$lvl < $self->{max_level}) {
+                    my $subtree = $node->[2][0];
+                    printf(" (%8x %8x)", $subtree, $subtree->[0]);
+                    push(@que, [$subtree, $lvl+1]);
+                } else {
+                    my $vals = $node->[2];
+                    printf(" (%08x ", $vals);
+                    map {  print '"',$_->[1], '",'} @$vals;
+                    print ")";
+                }
             } else {
                 printf(" (%8x %8x)", 0, 0);
             }
